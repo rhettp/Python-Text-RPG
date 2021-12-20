8369a7975a72198ceccfb4ba79735cee8b369878
Rhett <rp22@live.missouristate.edu>
2021-12-20 13:28:19 -0600

    Created basic title screen functions

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ import sys
 import os
 import time
 import random
+from title_screen import *
+
+##### Main Function #####
+def main():
+    title_screen()
+
+if __name__=="__main__":
+    main()
--- title_screen.py
+++ title_screen.py
@@ -4,3 +4,43 @@ import sys
 import os
 import time
 import random
+
+##### Title Screen #####
+def title_screen():
+    os.system('clear')
+    print('###################################')
+    print('# Welcome to the Python Text RPG! #')
+    print('###################################')
+    print('-Play-')
+    print('-Help-')
+    print('-Quit-')
+    title_screen_selections()
+
+##### Title Selections #####
+def title_screen_selections():
+    while True:
+        option = input('> ').lower()
+        if option == 'play':
+            #TODO
+            print("Play")
+            #start_game()
+            break
+        elif option == 'help':
+            help_menu()
+            break
+        elif option == 'quit':
+            sys.exit()
+        else:
+            print("Please enter a valid command.")
+            continue
+
+##### Help Menu #####
+def help_menu():
+    print("#############")
+    print("# Help Menu #")
+    print("#############")
+    print("Type left, right, up, or down to move.")
+    print("Type examine to investigate the room.")
+    print("Type quit to exit the game.")
+    print("Have fun!")
+    title_screen_selections()
