96c80ab75200b852de6db9318a8c13577095f458
Rhett <rp22@live.missouristate.edu>
2021-12-20 12:37:49 -0600

    added player.py

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Rhett Proctor
+Python Text RPG
+"""
+
+import cmd
+import textwrap
+import sys
+import os
+import time
+import random
---
+++ player.py
@@ -0,0 +1,15 @@
+import cmd
+import textwrap
+import sys
+import os
+import time
+import random
+
+##### Player setup #####
+class player:
+    def __init__(self):
+        self.name = ''
+        self.hp = 0
+        self.mp = 0
+        self.status_effects = []
+myPlayer = player()
